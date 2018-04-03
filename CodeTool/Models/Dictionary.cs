b12420bab7271895073f13eddd8817c5f57f57c3
liangwei0101 <liangwei0101@users.noreply.github.com>
2018-04-03 09:07:07 +0800

    增加相关翻译和标准字段类型

---
+++ CodeTool/Models/Dictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTool.Models
+{
+    public class Dictionary
+    {
+
+        public string Name;
+
+        public string Cname;
+
+        public string HsType;
+
+        public string Type;
+    }
+}
